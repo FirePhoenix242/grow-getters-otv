53ddbf68745cae0386262b9ca70bdb90285c0d9b
FirePhoenix242 <phoenix.a.shirley@gmail.com>
2025-10-22 14:53:27 -0400

    main i think

--- main.cpp
+++ main.cpp
@@ -2,7 +2,10 @@ using namespace std;
 #include <iostream>
 
 int main(){
-    for(int n = 0; n < 67; n++)
+    for(int n = 0; n < 67; n++){
         cout << "Penis ";
+        int penis;
+        cin >> penis;
+    }
     return 0;
 }
