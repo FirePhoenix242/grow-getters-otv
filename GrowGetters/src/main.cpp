df47491eda31c227365d3d76f3683c7a991152ad
FirePhoenix242 <phoenix.a.shirley@gmail.com>
2025-10-27 15:32:42 -0400

    Pins Fixed

--- GrowGetters/src/main.cpp
+++ GrowGetters/src/main.cpp
@@ -5,24 +5,23 @@
 //Pin Assignments
 const int WIFI_TRANSFER = 0;
 const int WIFI_RECIEVING = 1;
-const int FRONT_MOTOR_R_EN = 2;
-const int FRONT_MOTOR_R_PWN = 3;
-const int FRONT_MOTOR_L_EN = 4;
-const int FRONT_MOTOR_L_PWN = 5;
-const int BACK_MOTOR_R_PWN = 6;
-const int BACK_MOTOR_R_EN = 7;
-const int BACK_MOTOR_L_EN = 8;
-const int BACK_MOTOR_L_PWN = 9;
-const int MISSION_MOTOR_PWN = 10;
-const int MISSION_ACTUATOR_PWN = 11;
-const int MISSION_MOTOR_EN = 12;
-const int MISSION_ACTUATOR_EN = 13;
+const int FRONT_MOTOR_R_F = 2;
+const int FRONT_MOTOR_R_B = 3;
+const int FRONT_MOTOR_L_F = 4;
+const int FRONT_MOTOR_L_B = 5;
+const int BACK_MOTOR_R_F = 6;
+const int BACK_MOTOR_R_B = 7;
+const int BACK_MOTOR_L_F = 8;
+const int BACK_MOTOR_L_B = 9;
+const int MISSION_MOTOR_F = 10;
+const int MISSION_ACTUATOR_F = 11;
+const int MISSION_MOTOR_B = 12;
+const int MISSION_ACTUATOR_B = 13;
 const int ULTRASONIC_1_TRIG = A0;
 const int ULTRASONIC_1_ECHO = A1;
 const int ULTRASONIC_2_TRIG = A2;
 const int ULTRASONIC_2_ECHO = A3;
-const int PROPULSION_ACTUATOR_SPEED = A4;
-const int CLAW_SPEED = A5;
+const int CLAW_SPEED = 11;
 
 //Function Prototypes
 void moveForward(int);
@@ -37,23 +36,22 @@ void setup(){
   Enes100.begin("Grow Getters", SEED, 3, 1120, WIFI_TRANSFER, WIFI_RECIEVING);
   pinMode(WIFI_TRANSFER, OUTPUT);
   pinMode(WIFI_RECIEVING, INPUT);
-  pinMode(FRONT_MOTOR_R_EN, OUTPUT);
-  pinMode(FRONT_MOTOR_R_PWN, OUTPUT);
-  pinMode(FRONT_MOTOR_L_EN, OUTPUT);
-  pinMode(FRONT_MOTOR_R_PWN, OUTPUT);
-  pinMode(BACK_MOTOR_R_EN, OUTPUT);
-  pinMode(BACK_MOTOR_R_PWN, OUTPUT);
-  pinMode(BACK_MOTOR_L_EN, OUTPUT);
-  pinMode(BACK_MOTOR_L_PWN, OUTPUT);
-  pinMode(MISSION_MOTOR_EN, OUTPUT);
-  pinMode(MISSION_MOTOR_PWN, OUTPUT);
-  pinMode(MISSION_ACTUATOR_EN, OUTPUT);
-  pinMode(MISSION_ACTUATOR_PWN, OUTPUT);
+  pinMode(FRONT_MOTOR_R_F, OUTPUT);
+  pinMode(FRONT_MOTOR_R_B, OUTPUT);
+  pinMode(FRONT_MOTOR_L_F, OUTPUT);
+  pinMode(FRONT_MOTOR_R_B, OUTPUT);
+  pinMode(BACK_MOTOR_R_F, OUTPUT);
+  pinMode(BACK_MOTOR_R_B, OUTPUT);
+  pinMode(BACK_MOTOR_L_F, OUTPUT);
+  pinMode(BACK_MOTOR_L_B, OUTPUT);
+  pinMode(MISSION_MOTOR_F, OUTPUT);
+  pinMode(MISSION_MOTOR_B, OUTPUT);
+  pinMode(MISSION_ACTUATOR_F, OUTPUT);
+  pinMode(MISSION_ACTUATOR_B, OUTPUT);
   pinMode(ULTRASONIC_1_ECHO, INPUT);
   pinMode(ULTRASONIC_1_TRIG, INPUT);
   pinMode(ULTRASONIC_2_ECHO, INPUT);
   pinMode(ULTRASONIC_2_TRIG, INPUT);
-  pinMode(PROPULSION_ACTUATOR_SPEED, OUTPUT);
   pinMode(CLAW_SPEED, OUTPUT);
 }
 
