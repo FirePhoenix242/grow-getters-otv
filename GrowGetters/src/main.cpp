23f4343df4ed327419896bbcffc03faf8f78422a
FirePhoenix242 <phoenix.a.shirley@gmail.com>
2025-10-29 15:00:51 -0400

    MS5 Coded

--- GrowGetters/src/main.cpp
+++ GrowGetters/src/main.cpp
@@ -23,13 +23,22 @@ const int ULTRASONIC_2_TRIG = A2;
 const int ULTRASONIC_2_ECHO = A3;
 const int CLAW_SPEED = 11;
 
+//Global Constants
+const int MS5 = 2;
+
 //Function Prototypes
-void moveForward(int);
-void moveBackwards(int);
-void moveRight(int);
-void moveLeft(int);
-void turnRight(int);
-void turnLeft(int);
+void moveForward(float);
+void moveBackwards(float);
+void moveRight(float);
+void moveLeft(float);
+void turnRight(float);
+void turnLeft(float);
+float readUltrasonic1();
+float readUltrasonic2();
+void ms5(int);
+void ms5Sub23();
+void ms5Sub458();
+void ms5Sub6();
 
 void setup(){
   //Initializing Pins
@@ -56,23 +65,115 @@ void setup(){
 }
 
 void loop(){
+  ms5(MS5);
 }
 
 //Functions
-void moveForward(int dist){
+void moveForward(float dist){
 }
 
-void moveBackwards(int dist){
+void moveBackwards(float dist){
 }
 
-void moveRight(int dist){
+void moveRight(float dist){
 }
 
-void moveLeft(int dist){
+void moveLeft(float dist){
 }
 
-void TurnRight(int dist){
+void TurnRight(float dist){
 }
 
-void turnLeft(int dist){
+void turnLeft(float dist){
+}
+
+float readUltrasonic1(){
+  digitalWrite(ULTRASONIC_1_TRIG, LOW);
+  delayMicroseconds(5);
+  digitalWrite(ULTRASONIC_1_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRASONIC_1_TRIG, LOW);
+ 
+  // Read the signal from the sensor
+  float duration = pulseIn(ULTRASONIC_1_ECHO, HIGH);
+  return (duration / 29.1);
+}
+
+float readUltrasonic2(){
+  digitalWrite(ULTRASONIC_2_TRIG, LOW);
+  delayMicroseconds(5);
+  digitalWrite(ULTRASONIC_2_TRIG, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(ULTRASONIC_2_TRIG, LOW);
+ 
+  // Read the signal from the sensor
+  float duration = pulseIn(ULTRASONIC_2_ECHO, HIGH);
+  return (duration / 29.1);
+}
+
+void ms5(int subTask){
+  switch(subTask){
+    case(2):
+    case(3):
+      ms5Sub23();
+      break;
+    case(4):
+    case(5):
+    case(8):
+      ms5Sub458();
+      break;
+    case(6):
+      ms5Sub6();
+      break;
+    default:
+      Enes100.print("Wrong number, dumbass.");
+      break;
+  }
+}
+
+void ms5Sub23(){
+  moveForward(350);
+  delay(10000);
+
+  for(int n = 0; n < 3; n++){
+    turnRight(90);
+    delay(1000);
+  }
+  for(int n = 0; n < 3; n++){
+    turnLeft(90);
+    delay(1000);
+  }
+}
+
+void ms5Sub458(){
+  switch(Enes100.isVisible()){
+    case(true):
+      Enes100.println("Current X value is: ");
+      Enes100.print(Enes100.getX());
+      Enes100.println("Current Y value is: ");
+      Enes100.print(Enes100.getY());
+      Enes100.println("Current heading value is: ");
+      Enes100.print(Enes100.getTheta());
+      Enes100.println("Ultrasonic 1 is reading: ");
+      Enes100.print(readUltrasonic1());
+      Enes100.print("cm");
+      Enes100.println("Ultrasonic 2 is reading: ");
+      Enes100.print(readUltrasonic2());
+      Enes100.print("cm");
+      delay(500);
+      break;
+    default:
+      Enes100.println("Aruco Marker is not visible.");
+      delay(1000);
+      break;
+  }
+
+}
+
+void ms5Sub6(){
+  if(Enes100.getTheta() >= 0)
+    turnRight(Enes100.getTheta());
+  else
+    turnLeft(-Enes100.getTheta());
+  ms5Sub23();
 }
